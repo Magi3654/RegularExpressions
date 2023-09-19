b2583e23c0efa443c895c60b988d2888d765801c
Magi3654 <112009244+Magi3654@users.noreply.github.com>
2023-09-18 23:36:30 -0700

    Clean btn and funtionality

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 const btn = document.querySelector("#clickBtn");
-const btn2 = document.querySelector("limpiarBtn");
+const btn2 = document.getElementById("limpiarBtn");
 const pTag = document.querySelector(" .result ");
 let generalInput = document.getElementById("generalInput");
-let myString = generalInput.value;
-const regPattern = RegExp("baja", "gimu");
+let expresionR = document.getElementById("expresion");
 btn.addEventListener("click", (e) => {
+  let myString = generalInput.value;
+  let expresionRV=expresionR.value;
+  const regPattern = RegExp(expresionRV, "gimu");
   const result = myString.match(regPattern);
   if (result) {
-    pTag.innerHTML = "Coincidencia: " + result[0];
+    pTag.innerHTML = "Coincidencia: "+ result;
   } else pTag.innerHTML = "Coincidencia: 0";
 });
 
-btn2.addEventListener("limpiar", (e) =>{
-  myString.value=" ";
-  result.value=" "
+limpiarBtn.addEventListener("click", () => {
+  generalInput.value = "";
+  expresionR.value = "";
+  pTag.innerHTML="";
 });
 
-////console.log(regPattern.test(myString));// boolean
-//console.log(regPattern.exec(myString));// este es el array
-console.log(myString);
-//console.log(result);
+// falta hponer el numero de conincidencias
+
+
+
