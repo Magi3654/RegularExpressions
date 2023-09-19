ce98b96fa6aea81697c64c43d6c56ad069573fbc
Magi3654 <112009244+Magi3654@users.noreply.github.com>
2023-09-18 22:59:38 -0700

    styles

--- main.js
+++ main.js
@@ -1,38 +1,22 @@
-import "./style.css";
+const btn = document.querySelector("#clickBtn");
+const btn2 = document.querySelector("limpiarBtn");
+const pTag = document.querySelector(" .result ");
+let generalInput = document.getElementById("generalInput");
+let myString = generalInput.value;
+const regPattern = RegExp("baja", "gimu");
+btn.addEventListener("click", (e) => {
+  const result = myString.match(regPattern);
+  if (result) {
+    pTag.innerHTML = "Coincidencia: " + result[0];
+  } else pTag.innerHTML = "Coincidencia: 0";
+});
 
-//let patron = new RegExp('a$');
-// Cadena de input
-//const cadena = "aprendiendo expresiones regurales";
+btn2.addEventListener("limpiar", (e) =>{
+  myString.value=" ";
+  result.value=" "
+});
 
-//let patron = /expresiones/;
-
-const expRegular= document.createElement('input');
-expRegular.id="expresion";
-expRegular.placeholder="Inserta tu patron";
-
-const frase=document.createElement('input');
-frase.id="frase";
-
-
-const button=document.createElement('button');
-button.textContent="Check";
-button.onclick=RegExp;
-button.className='check'
-
-const regularExpGen=document.getElementById('expresion');
-
-let myPattern=RegExp(regularExpGen);
-const parrafo = document.createElement("p");
-
-if (myPattern.test(frase)) {
-  parrafo.textContent = "Si hizo match";
-} else {
-  parrafo.textContent = "No hizo match";
-}
-
-
-expresiones.appendChild(expRegular);
-expresiones.appendChild(frase);
-expresiones.appendChild(button);
-document.body.appendChild(parrafo);
-
+////console.log(regPattern.test(myString));// boolean
+//console.log(regPattern.exec(myString));// este es el array
+console.log(myString);
+//console.log(result);
